f2591edfa22bcf630754229f78ee9b532c4beb19
Vladislav-web <ykt.pc_master@mail.ru>
2021-01-18 17:09:49 +0300

    added JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,73 @@
+var name = "Ivan";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth;
+
+// number
+// string -"", '',``
+// true/false 
+// null 
+// undefiend
+// let obj = {
+//   name: 'apple',
+//   color: 'green',
+//   weight: 200
+// }
+// Symbol 
+
+// alert(123124)
+// console.log(number);
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// console.log(4 + 'asdfsad');
+
+// let isChecked = false,
+//   isClose = false;
+
+  // console.log(isChecked && isClose);
+
+  // console.log(isChecked || isClose);
+
+// if (2 * 6 == 8 * 1) {
+//   console.log('Верно');
+// } else {
+//   console.log('Ошибка');
+// }
+
+// let answer = confirm("Вам есть 18?");
+// if (answer) {
+//   console.log('Проходите');
+// } else {
+//   console.log('Уходи');
+// }
+
+// const num = 50
+
+// if (num < 49) {
+//   console.log('Это неправильно');
+// } else if (num > 100) {
+//   console.log('Много');
+// } else {
+//   console.log('Верно');
+// }
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i);
+// }
+
+function loging(a, b) {
+  console.log(a + b);
+}
+
+loging(3, 5);
+
+loging(6, 8);
+
