adbe64d6adefa5e8979236918a73d3abe8127a87
Vladislav-web <ykt.pc_master@mail.ru>
2021-01-21 18:41:11 +0300

    added validate and mask

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,66 @@ $(document).ready(function () {
       $('.overlay, #order').fadeIn('slow');
     })
   });
+
+  //! Validate
+  
+  // $('#order form').validate();
+  // $('#consultation-form').validate();
+  // $('#consultation form').validate({
+  //   rules: {
+  //     name: {
+  //       required: true,
+  //       minlength: 2
+  //     },
+  //     phone: "required",
+  //     email: {
+  //       required: true,
+  //       email: true
+  //     }
+  //   },
+  //   messages: {
+  //     name: {
+  //       required: "Пожалуйста, введите свое имя",
+  //       minlength: jQuery.validator.format("Введите {0} символа!")
+  //     },
+  //     phone: "Пожалуйста, введите свой номер телефона ",
+  //     email: {
+  //       required: "Пожалкйста, введите свою почту",
+  //       email: "Неправильно введен адрес почты"
+  //     }
+  //   }
+  // });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона ",
+        email: {
+          required: "Пожалкйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  }
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  $('input[name=phone]').mask("+7 (999) 999-9999");
 });
